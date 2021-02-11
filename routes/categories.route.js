634ffbd2963a013274d94397f6d9fc9b83646336
Julien PICQUET <dext.1234@gmail.com>
2021-02-11 13:51:48 +0100

    commit route all

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -1,6 +1,7 @@
 module.exports = app => {
     const categorie = require("../controllers/categorie.controller");
-  
+    const evenement = require("../controllers/evenements.controller");
+    
     var router = require("express").Router();
   
     router.post("/categories", categorie.create);
@@ -10,6 +11,13 @@ module.exports = app => {
     router.put("/categories/:id", categorie.update);
     router.delete("/categories/:id", categorie.delete);
     router.delete("/categories", categorie.deleteAll);
+
+    router.post("/evenements", evenement.create);
+    router.get("/evenements", evenement.findAll);
+    router.get("/evenements/:id", evenement.findOne);
+    router.put("/evenements/:id", evenement.update);
+    router.delete("/evenements/:id", evenement.delete);
+    router.delete("/evenements", evenement.deleteAll);
 
     app.use('/api/', router);
   };
