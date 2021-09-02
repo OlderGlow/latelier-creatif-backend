19d6c5ee4c3848c55dd172b90f5605727419e43c
Julien Picquet <julienpicquet1@gmail.com>
2021-09-02 10:33:55 +0200

    delete route for delete all http request

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -10,7 +10,6 @@ module.exports = app => {
     router.get("/categories/:id", categorie.findOne);
     router.put("/categories/:id", categorie.update);
     router.delete("/categories/:id", categorie.delete);
-    router.delete("/categories", categorie.deleteAll);
 
     router.post("/evenements", evenement.create);
     router.get("/evenements", evenement.findAll);
@@ -18,7 +17,6 @@ module.exports = app => {
     router.get("/evenements/:id", evenement.findOne);
     router.put("/evenements/:id", evenement.update);
     router.delete("/evenements/:id", evenement.delete);
-    router.delete("/evenements", evenement.deleteAll);
 
     app.use('/api/', router);
   };
